692e74bc9a3963c9741b7b702e1098df9a7235a7
vi_ser <vinceulisse@gmail.com>
2024-04-09 12:15:42 +0200

    recuperati i film del database (per ora mostrati solo tramide dd)

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // funzione per gestire la rotta e recuperare tutti i movie
+    public function index()
+    {
+
+        $movies = Movie::all();
+
+        dd($movies);
+        return view('home');
+    }
+}
